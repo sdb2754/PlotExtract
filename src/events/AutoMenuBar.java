22ec941f8dbbe8761045d30665f25413ec73faca
sdb2754 <aeroengineer94@gmail.com>
2017-05-27 22:27:59 -0500

    updated and tested AutoMenuBar code. Figure code throwing errors during curve fitting.

---
+++ src/events/AutoMenuBar.java
@@ -0,0 +1,510 @@
+package events;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+import javax.swing.AbstractButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JRadioButtonMenuItem;
+
+public class AutoMenuBar extends JMenuBar implements ActionListener, ItemListener {
+	private static final long serialVersionUID = 1L;
+
+	// Button Groups
+	ArrayList<cGroup> groups = new ArrayList<cGroup>();
+
+	// Top level menus
+	ArrayList<cMenu> menus = new ArrayList<cMenu>();
+
+	// Constructor
+	AutoMenuBar() {
+
+	}
+
+	// Methods for adding items
+	/*
+	 * These methods take a name which is the display text. No two items may
+	 * have the same name.
+	 * 
+	 * 
+	 * the String "parent" is the name of the parent use "bar" to place a menu
+	 * at the top level only menus can be added to the top level
+	 * 
+	 * the String "type" indicated which type of item to add Items can be menus,
+	 * items, radio buttons, or check boxes This method is also used to add
+	 * ButtonGroups
+	 * 
+	 * the boolean "checked" is used if the item is a check box or radio button
+	 * to set initial value
+	 * 
+	 * Consumer<Boolean> action is a function that runs when the item is clicked
+	 * and/or state changed This can be set like:
+	 * 
+	 * Consumer<Boolean> action = (y) -> {//some Java code}
+	 * 
+	 * When:
+	 * 
+	 * action.accept(boolean);
+	 * 
+	 * is called, then the code is executed
+	 * 
+	 */
+	public void add_item(String name, String parent, String type, String group) {
+		add_item(name, parent, type, group, false, (y) -> {
+		});
+	}
+
+	public void add_item(String name, String parent, String type, String group, boolean checked) {
+		add_item(name, parent, type, group, checked, (y) -> {
+		});
+	}
+
+	public void add_item(String name, String parent, String type, Consumer<Boolean> action) {
+		add_item(name, parent, type, "none", false, action);
+	}
+
+	public void add_item(String name, String parent, String type, boolean checked) {
+		add_item(name, parent, type, "none", checked, (y) -> {
+		});
+	}
+
+	public void add_item(String name, String parent, String type) {
+		add_item(name, parent, type, "none", false, (y) -> {
+		});
+	}
+
+	// The main add_item method
+	public void add_item(String name, String parent, String type, String group, boolean checked,
+			Consumer<Boolean> action) {
+
+		// Get the parent Component
+		Component cp = get(parent);
+		
+		//If not match was found, return
+		if (cp == null)
+			return;
+		//If the type of parent isn't cMenu, then something went wrong
+		//Either a typo in the add_item call, or a redundant item name
+		//Return
+		if(!cp.getClass().equals(cMenu.class)&&cp != this)
+			return;
+		
+		switch (type) {
+
+		case "menu":
+			// if the parent is the MenuBar
+			if (cp == this) {
+				cMenu cm = new cMenu(name);
+				// add to MenuBar
+				this.add(cm);
+				// Listen for click
+				cm.addActionListener(this);
+				// Add to top level menu list
+				menus.add(cm);
+				// Set the click action
+				cm.set_action(action);
+				return;
+			}
+			cMenu cm = new cMenu(name);
+			// Add the item to its parent
+			((cMenu) cp).add_item(cm, name, type, group);
+			cm.addActionListener(this);
+			cm.set_action(action);
+			break;
+		case "item":
+			cItem ci = new cItem(name);
+			((cMenu) cp).add_item(ci, name, type, group);
+			ci.addActionListener(this);
+			ci.set_action(action);
+			break;
+		case "radio":
+			cRadio cr = new cRadio(name);
+			((cMenu) cp).add_item(cr, name, type, group);
+			cr.addActionListener(this);
+			cr.addItemListener(this);
+			// If the item should be added to a ButtonGroup
+			if (group != "none")
+				cr.group(group(group));
+			// Set initial check state
+			cr.setSelected(checked);
+			cr.set_action(action);
+			break;
+		case "check":
+			cCheck cc = new cCheck(name);
+			((cMenu) cp).add_item(cc, name, type, group);
+			cc.addActionListener(this);
+			cc.addItemListener(this);
+			cc.setSelected(checked);
+			cc.set_action(action);
+			break;
+		case "group":
+			// Add a new ButtonGroup
+			groups.add(new cGroup(name));
+			break;
+
+		}
+	}
+
+	// Set the click action for an item
+	public void set_action(String name, Consumer<Boolean> a) {
+
+		// Get the Component named "name"
+		Component c = get(name);
+
+		if (c == null)
+			return;
+		// Must handle each type separately
+		// because the Component can only be cast to one type
+		if (c.getClass().equals(cMenu.class)) {
+			// Calls the set_action method for the menu item
+			((cMenu) c).set_action(a);
+			return;
+		}
+		if (c.getClass().equals(cItem.class)) {
+			((cItem) c).set_action(a);
+			return;
+		}
+		if (c.getClass().equals(cCheck.class)) {
+			((cCheck) c).set_action(a);
+			return;
+		}
+		if (c.getClass().equals(cRadio.class)) {
+			((cRadio) c).set_action(a);
+			return;
+		}
+	}
+
+	// Set the state changed action for an item
+	public void set_statechange(String name, Consumer<Boolean> a) {
+
+		// Get the Component named "name"
+		Component c = get(name);
+		// Must handle each type separately
+		// because the Component can only be cast to one type
+		if (c.getClass().equals(cCheck.class)) {
+			((cCheck) c).set_statechanged(a);
+			return;
+		}
+		if (c.getClass().equals(cRadio.class)) {
+			((cRadio) c).set_statechanged(a);
+			return;
+		}
+	}
+
+	// Get item by name
+	private Component get(String name) {
+
+		// return the MenuBar
+		if (name == "bar")
+			return this;
+		// Check if item is in top level menus
+		for (cMenu cm : menus) {
+			if (cm.name.equals(name))
+				return cm;
+		}
+		// Allocate an empty Component
+		Component cp = null;
+		// Recursively search menus inside top level menus
+		for (cMenu cm : menus) {
+			// Calls the "get" method for a menu item
+			cp = cm.get(name);
+			// If it found a match
+			if (cp != null)
+				// return the Component
+				return cp;
+		}
+		// Nothing found. Return null
+		return null;
+	}
+
+	// get ButtonGroup by name
+	private cGroup group(String name) {
+		// Search list of ButtonGroups
+		for (cGroup cg : groups) {
+			// If a match is found
+			if (cg.name.equals(name)) {
+				// return the match
+				return cg;
+			}
+		}
+		// Nothing found. Return null
+		return null;
+	}
+
+	// Custom JMenu class
+	private class cMenu extends JMenu {
+		private static final long serialVersionUID = 1L;
+		// The display name
+		String name;
+		// The click action
+		Consumer<Boolean> action;
+		// Lists of items that are in this menu
+		ArrayList<cMenu> menus = new ArrayList<cMenu>();
+		ArrayList<cItem> items = new ArrayList<cItem>();
+		ArrayList<cCheck> checkboxes = new ArrayList<cCheck>();
+		ArrayList<cRadio> radiobuttons = new ArrayList<cRadio>();
+
+		// Constructor
+		public cMenu(String s) {
+			// Set display name
+			name = s;
+			this.setText(name);
+			// Initialize action with an empty function
+			action = (y) -> {
+			};
+		}
+
+		// Get member item by name
+		public Component get(String name) {
+			// Search menus
+			for (cMenu cm : menus) {
+				if (cm.name.equals(name))
+					return cm;
+			}
+			// Search items
+			for (cItem ci : items) {
+				if (ci.name.equals(name))
+					return ci;
+			}
+			// Search check boxes
+			for (cCheck cc : checkboxes) {
+				if (cc.name.equals(name))
+					return cc;
+			}
+			// Search radio buttons
+			for (cRadio cr : radiobuttons) {
+				if (cr.name.equals(name))
+					return cr;
+			}
+			// Recursively search menus inside this menu
+			Component cp = null;
+			for (cMenu cm : menus) {
+				cp = cm.get(name);
+				if (cp != null)
+					return cp;
+			}
+			// Nothing found
+			return null;
+		}
+
+		// Add a new item to this menu
+		public void add_item(Component c, String name, String type, String group) {
+			// The item has already been configured in the top level "add_item"
+			// method
+			// So, all we have to do is add it to the appropriate list and to
+			// the Menu itself
+			switch (type) {
+
+			case "menu":
+				menus.add((cMenu) c);
+				break;
+			case "item":
+				items.add((cItem) c);
+				break;
+			case "radio":
+				radiobuttons.add((cRadio) c);
+				break;
+			case "check":
+				checkboxes.add((cCheck) c);
+				break;
+			}
+
+			// Add to the menu
+			add(c);
+		}
+
+		// Setter method for click action
+		public void set_action(Consumer<Boolean> c) {
+			action = c;
+		}
+
+		// Runs the "action" function, with a boolean argument "b"
+		public void act(boolean b) {
+			action.accept(b);
+		}
+
+	}
+
+	// Custom JMenuItem class
+	private class cItem extends JMenuItem {
+		private static final long serialVersionUID = 1L;
+		// Display name
+		String name;
+		// Click action
+		Consumer<Boolean> action;
+
+		// Constructor
+		public cItem(String s) {
+			name = s;
+			this.setText(name);
+
+			action = (y) -> {
+			};
+
+		}
+
+		// Set click action
+		public void set_action(Consumer<Boolean> c) {
+			action = c;
+		}
+
+		// Run click action
+		public void act(boolean b) {
+			action.accept(b);
+		}
+
+	}
+
+	// Custom JCheckBoxMenuItem class
+	private class cCheck extends JCheckBoxMenuItem {
+		private static final long serialVersionUID = 1L;
+		// Display name
+		String name;
+		// Click action
+		Consumer<Boolean> action;
+		Consumer<Boolean> statechanged;
+
+		// Constructor
+		public cCheck(String s) {
+			name = s;
+			this.setText(name);
+			action = (y) -> {
+			};
+			statechanged = (y) -> {
+			};
+		}
+
+		// Set click action
+		public void set_action(Consumer<Boolean> c) {
+			action = c;
+		}
+
+		public void set_statechanged(Consumer<Boolean> c) {
+			statechanged = c;
+		}
+
+		// Run click action
+		public void act(boolean b) {
+			action.accept(b);
+		}
+
+		public void state(boolean b) {
+			statechanged.accept(b);
+		}
+	}
+
+	// Custom JRadioButtonMenuItem class
+	private class cRadio extends JRadioButtonMenuItem {
+		private static final long serialVersionUID = 1L;
+		// Display name
+		String name;
+		// Click action
+		Consumer<Boolean> action;
+		Consumer<Boolean> statechanged;
+
+		public cRadio(String s) {
+			name = s;
+			this.setText(name);
+			action = (y) -> {
+			};
+			statechanged = (y) -> {
+			};
+		}
+
+		// Add this to ButtonGroup bg
+		public void group(cGroup bg) {
+			bg.add(this);
+		}
+
+		// Set click action
+		public void set_action(Consumer<Boolean> c) {
+			action = c;
+		}
+
+		public void set_statechanged(Consumer<Boolean> c) {
+			statechanged = c;
+		}
+
+		// Run click action
+		public void act(boolean b) {
+			action.accept(b);
+		}
+
+		public void state(boolean b) {
+			statechanged.accept(b);
+		}
+	}
+
+	// Custom ButtonGroup class
+	private class cGroup extends ButtonGroup {
+		private static final long serialVersionUID = 1L;
+		// Reference name
+		String name;
+
+		public cGroup(String s) {
+			name = s;
+		}
+	}
+
+	// Click event
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// Get the display name of the clicked item
+		String cmd = e.getActionCommand();
+		// Get the item by name
+		Component c = get(cmd);
+		// Must handle each type separately because
+		// you can only cast Component to one type
+		if (c.getClass().equals(cMenu.class)) {
+			// Run click action
+			((cMenu) c).act(true);
+			return;
+		}
+		if (c.getClass().equals(cItem.class)) {
+			((cItem) c).act(true);
+			return;
+		}
+		if (c.getClass().equals(cCheck.class)) {
+			((cCheck) c).act(true);
+			return;
+		}
+		if (c.getClass().equals(cRadio.class)) {
+			((cRadio) c).act(true);
+			return;
+		}
+
+	}
+
+	// Item state (e.g. checked) is changed
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		// Abstract button to determine checked state
+		AbstractButton button = (AbstractButton) e.getItem();
+		// state of item
+		boolean state = button.isSelected();
+		// Get the display name
+		String cmd = button.getText();
+		// Get item by name
+		Component c = get(cmd);
+		// Must handle each type separately because
+		// you can only cast Component to one type
+		if (c.getClass().equals(cCheck.class)) {
+			((cCheck) c).state(state);
+			return;
+		}
+		if (c.getClass().equals(cRadio.class)) {
+			((cRadio) c).state(state);
+			return;
+		}
+	}
+
+}
--- src/events/Board.java
+++ src/events/Board.java
@@ -8,241 +8,275 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
-import java.util.function.Consumer;
-
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
-public class Board extends JPanel implements MouseMotionListener, MouseListener, KeyListener
- {
-   private static final long serialVersionUID = 1L;
-   static JFrame frame;
-   static long dt;
-   static Board newContentPane;
-   
-   CustomMenuBar ribbon;
-   static Figure figure;
-   static Table table;
-   
-   static JTabbedPane tabbedPane = new JTabbedPane();
- 
-   public static void main(String[] args)
-   {
-     frame = new JFrame("Plot Extractor");
-     frame.setDefaultCloseOperation(3);
+public class Board extends JPanel implements MouseMotionListener, MouseListener, KeyListener {
+	private static final long serialVersionUID = 1L;
+	static JFrame frame;
+	static long dt;
+	static Board newContentPane;
 
-     newContentPane = new Board();
-     newContentPane.setOpaque(true);
-     frame.setContentPane(newContentPane);
-    frame.setPreferredSize(new Dimension(1400, 1000)); 
-     frame.pack();
-     frame.setResizable(false);
-     frame.setVisible(true);
+	AutoMenuBar ribbon;
+	static Figure figure;
+	static Table table;
 
+	static JTabbedPane tabbedPane = new JTabbedPane();
 
-     dt = 10;
-     while (true)
-     {
-       update((int)dt);
- 
-       newContentPane.repaint();
-      try
-      {
-        Thread.sleep(dt);
-      }
-       catch (InterruptedException e) {
-         e.printStackTrace();
-      }
-    }
-  }
+	public static void main(String[] args) {
+		frame = new JFrame("Plot Extractor");
+		frame.setDefaultCloseOperation(3);
 
-   public Board()
-   {
-     setLayout(new BorderLayout());
-     setFocusable(true);
-     addKeyListener(this);
-     
-     figure = new Figure();
-     table = new Table(50,20);
-     
-     add(tabbedPane,BorderLayout.CENTER);
-     
-     tabbedPane.add("Figure", figure);
-     tabbedPane.add("Table", table);
+		newContentPane = new Board();
+		newContentPane.setOpaque(true);
+		frame.setContentPane(newContentPane);
+		frame.setPreferredSize(new Dimension(1400, 1000));
+		frame.pack();
+		frame.setResizable(false);
+		frame.setVisible(true);
 
-     figure.addMouseListener(this);
-     figure.addMouseMotionListener(this);
-     
-     ribbon = new CustomMenuBar();
-     add(ribbon,BorderLayout.NORTH);
-     configure_menu();
-   }
+		dt = 10;
+		while (true) {
+			update((int) dt);
 
-  private static void update(int dt)
-  {
-
-  }
-  
-  private void configure_menu(){
-	   //Menus
-	  ribbon.add_item("File","bar","menu");
-	  ribbon.add_item("Edit","bar","menu");
-	  ribbon.add_item("Help","bar","menu");
-	  ribbon.add_item("Layers","bar","menu");
-	  ribbon.add_item("Fit","bar","menu");
-	   
-	   //Items
-	  ribbon.add_item("Reset","File","item");
-	  ribbon.add_item("Open","File","item");
-	  ribbon.add_item("Export","File","item");
-	  ribbon.add_item("Paste","Edit","item");
-	  ribbon.add_item("Copy","Edit","item");
-	  ribbon.add_item("About","Help","item");
-	  ribbon.add_item("Controls","Help","item");
-	   
-	   //Checkboxes
-	  ribbon.add_item("Original plot","Layers","check");
-	  ribbon.add_item("Origin","Layers","check");
-	  ribbon.add_item("X calibration","Layers","check");
-	  ribbon.add_item("Y calibration","Layers","check");
-	  ribbon.add_item("Data points","Layers","check");
-	  ribbon.add_item("Fit","Layers","check");
-	   
-	   //radiobuttons
-	  ribbon.add_item("radios","bar","group");
-	  ribbon.add_item("Linear","Fit","radio","radios");
-	  ribbon.add_item("Interpolation","Fit","radio","radios");
-	  ribbon.add_item("Spline","Fit","radio","radios");
-	  ribbon.add_item("Connect","Fit","radio","radios");
-	  ribbon.add_item("Regression","Fit","radio","radios");
-	  ribbon.add_item("None","Fit","radio","radios");
-	  
-	  //actions
-	  ribbon.set_action("Reset",  (y) -> {figure.set_step(0);});
-	  ribbon.set_action("Open",  (y) -> {figure.addimage();});
-	  ribbon.set_action("Paste",  (y) -> {if(tabbedPane.getSelectedIndex()==0)figure.getImageFromClipboard();});
-	  ribbon.set_action("Copy",  (y) -> {if(tabbedPane.getSelectedIndex()==1)table.copytoClipboard();});
-	  ribbon.set_action("Export",  (y) -> {figure.export();});
-	  ribbon.set_action("About",  (y) -> {about();});
-	  ribbon.set_action("Controls",  (y) -> {controls();});
-	  ribbon.set_action("Regression",  (y) -> {if(y)figure.set_fit("regression");});
-	  
-	  //state changes
-	  ribbon.set_action("Original plot",  (y) -> {figure.showplot=y;});
-	  ribbon.set_action("Fit",  (y) -> {figure.showfit=y;});
-	  ribbon.set_action("Origin",  (y) -> {figure.showorigin=y;});
-	  ribbon.set_action("X calibration",  (y) -> {figure.showx=y;});
-	  ribbon.set_action("Y calibration",  (y) -> {figure.showy=y;});
-	  ribbon.set_action("Data points",  (y) -> {figure.showdata=y;});
-	  ribbon.set_action("Linear",  (y) -> {if(y)figure.set_fit("linear");});
-	  ribbon.set_action("Interpolation",  (y) -> {if(y)figure.set_fit("interp");});
-	  ribbon.set_action("Spline",  (y) -> {if(y)figure.set_fit("spline");});
-	  ribbon.set_action("Connect",  (y) -> {if(y)figure.set_fit("connect");});
-	  ribbon.set_action("None",  (y) -> {if(y)figure.set_fit("none");});
-  }
-  
-  public void about(){
-	  JOptionPane.showMessageDialog(Board.frame, "PlotExtract\nBy Seth Berry.\nvs. 1.6\nExtracts data from plots and figures.");
-  }
-  public void controls(){
-	  JOptionPane.showMessageDialog(Board.frame, 
-				"To begin, add a plot, graph, figure, etc.\n"
-				+ "You can use File+Paste, cntrl+v,\n"
-				+ " or click the plot area to add a picture from a file.\n\n"
-				+ "Follow the instructions at the bottom of the window\n"
-				+ "to add calibration points. Click an axis calibration\n"
-				+ "point to see an option tochange that axis to a log axis.\n\n"
-				+ "Rightclick a point to see a list of actions you can perform on it.\n"
-				+ "Leftclick a point enable moving it.\n"
-				+ "Move a point using the arrow keys.\n\n"
-				+ "Change the fit type by clicking a datapoint and selecting 'fit'\n"
-				+ "Export by using File+Export or clicking a datapoint\n"
-				+ "and selecting 'Export'.\n\n"
-				+ "Datapoints can be moved or deleted.");
-  }
-
-@Override
-public void keyPressed(KeyEvent e) {
-	// TODO Auto-generated method stub
-	// TODO Auto-generated method stub
-	if ((e.getKeyCode() == KeyEvent.VK_V) && ((e.getModifiers() & KeyEvent.CTRL_MASK) != 0 && tabbedPane.getSelectedIndex()==0)) {
-        figure.getImageFromClipboard();
-        return;
-    }
-	switch(e.getKeyCode()){
-	
-	case KeyEvent.VK_UP: 
-		figure.movepoint(0,-1);
-		break;
-	case KeyEvent.VK_DOWN: 
-		figure.movepoint(0,1);
-		break;
-	case KeyEvent.VK_LEFT: 
-		figure.movepoint(-1,0);
-		break;
-	case KeyEvent.VK_RIGHT: 
-		figure.movepoint(1,0);
-		break;
-		
-	
+			newContentPane.repaint();
+			try {
+				Thread.sleep(dt);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
-}
 
-@Override
-public void keyReleased(KeyEvent arg0) {
-	// TODO Auto-generated method stub
-	
-}
+	public Board() {
+		setLayout(new BorderLayout());
+		setFocusable(true);
+		addKeyListener(this);
 
-@Override
-public void keyTyped(KeyEvent arg0) {
-	// TODO Auto-generated method stub
-	
-}
+		figure = new Figure();
+		table = new Table(50, 20);
 
-@Override
-public void mouseClicked(MouseEvent e) {
-	// TODO Auto-generated method stub
-	boolean isrightbutton=(e.getModifiers() & InputEvent.BUTTON3_MASK)
-			== InputEvent.BUTTON3_MASK;
-	figure.addpoint(e.getX(), e.getY(),isrightbutton);
-}
+		add(tabbedPane, BorderLayout.CENTER);
 
-@Override
-public void mouseEntered(MouseEvent arg0) {
-	// TODO Auto-generated method stub
-	
-}
+		tabbedPane.add("Figure", figure);
+		tabbedPane.add("Table", table);
 
-@Override
-public void mouseExited(MouseEvent arg0) {
-	// TODO Auto-generated method stub
-	
-}
+		figure.addMouseListener(this);
+		figure.addMouseMotionListener(this);
 
-@Override
-public void mousePressed(MouseEvent arg0) {
-	// TODO Auto-generated method stub
-	
-}
+		ribbon = new AutoMenuBar();
+		add(ribbon, BorderLayout.NORTH);
+		configure_menu();
+	}
 
-@Override
-public void mouseReleased(MouseEvent arg0) {
-	// TODO Auto-generated method stub
-	
-}
+	private static void update(int dt) {
 
-@Override
-public void mouseDragged(MouseEvent arg0) {
-	// TODO Auto-generated method stub
-	
-}
+	}
 
-@Override
-public void mouseMoved(MouseEvent e) {
-	// TODO Auto-generated method stub
+	private void configure_menu() {
+		// Menus
+		ribbon.add_item("File", "bar", "menu");
+		ribbon.add_item("Edit", "bar", "menu");
+		ribbon.add_item("Help", "bar", "menu");
+		ribbon.add_item("Layers", "bar", "menu");
+		ribbon.add_item("Fit type", "bar", "menu");
+
+		// Items
+		ribbon.add_item("Reset", "File", "item");
+		ribbon.add_item("Open", "File", "item");
+		ribbon.add_item("Export", "File", "item");
+		ribbon.add_item("Paste", "Edit", "item");
+		ribbon.add_item("Copy", "Edit", "item");
+		ribbon.add_item("About", "Help", "item");
+		ribbon.add_item("Controls", "Help", "item");
+
+		// Checkboxes
+		ribbon.add_item("Original plot", "Layers", "check",true);
+		ribbon.add_item("Origin", "Layers", "check",true);
+		ribbon.add_item("X calibration", "Layers", "check",true);
+		ribbon.add_item("Y calibration", "Layers", "check",true);
+		ribbon.add_item("Data points", "Layers", "check",true);
+		ribbon.add_item("Fit", "Layers", "check",true);
+
+		// radiobuttons
+		ribbon.add_item("radios", "bar", "group");
+		ribbon.add_item("Linear", "Fit type", "radio", "radios");
+		ribbon.add_item("Interpolation", "Fit type", "radio", "radios");
+		ribbon.add_item("Spline", "Fit type", "radio", "radios");
+		ribbon.add_item("Connect", "Fit type", "radio", "radios");
+		ribbon.add_item("Regression", "Fit type", "radio", "radios");
+		ribbon.add_item("None", "Fit type", "radio", "radios",true);
+
+		// actions
+		ribbon.set_action("Reset", (y) -> {
+			figure.set_step(0);
+		});
+		ribbon.set_action("Open", (y) -> {
+			figure.addimage();
+		});
+		ribbon.set_action("Paste", (y) -> {
+			if (tabbedPane.getSelectedIndex() == 0)
+				figure.getImageFromClipboard();
+		});
+		ribbon.set_action("Copy", (y) -> {
+			if (tabbedPane.getSelectedIndex() == 1)
+				table.copytoClipboard();
+		});
+		ribbon.set_action("Export", (y) -> {
+			figure.export();
+		});
+		ribbon.set_action("About", (y) -> {
+			about();
+		});
+		ribbon.set_action("Controls", (y) -> {
+			controls();
+		});
+		ribbon.set_action("Regression", (y) -> {
+			if (y)
+				figure.set_fit("regression");
+		});
+
+		// state changes
+		ribbon.set_statechange("Original plot", (y) -> {
+			figure.showplot = y;
+		});
+		ribbon.set_statechange("Fit", (y) -> {
+			figure.showfit = y;
+		});
+		ribbon.set_statechange("Origin", (y) -> {
+			figure.showorigin = y;
+		});
+		ribbon.set_statechange("X calibration", (y) -> {
+			figure.showx = y;
+		});
+		ribbon.set_statechange("Y calibration", (y) -> {
+			figure.showy = y;
+		});
+		ribbon.set_statechange("Data points", (y) -> {
+			figure.showdata = y;
+		});
+		ribbon.set_action("Linear", (y) -> {
+			if (y)
+				figure.set_fit("linear");
+		});
+		ribbon.set_action("Interpolation", (y) -> {
+			if (y)
+				figure.set_fit("interp");
+		});
+		ribbon.set_action("Spline", (y) -> {
+			if (y)
+				figure.set_fit("spline");
+		});
+		ribbon.set_action("Connect", (y) -> {
+			if (y)
+				figure.set_fit("connect");
+		});
+		ribbon.set_action("None", (y) -> {
+			if (y)
+				figure.set_fit("none");
+		});
+	}
+
+	public void about() {
+		JOptionPane.showMessageDialog(Board.frame,
+				"PlotExtract\nBy Seth Berry.\nvs. 1.6\nExtracts data from plots and figures.");
+	}
+
+	public void controls() {
+		JOptionPane.showMessageDialog(Board.frame,
+				"To begin, add a plot, graph, figure, etc.\n" + "You can use File+Paste, cntrl+v,\n"
+						+ " or click the plot area to add a picture from a file.\n\n"
+						+ "Follow the instructions at the bottom of the window\n"
+						+ "to add calibration points. Click an axis calibration\n"
+						+ "point to see an option tochange that axis to a log axis.\n\n"
+						+ "Rightclick a point to see a list of actions you can perform on it.\n"
+						+ "Leftclick a point enable moving it.\n" + "Move a point using the arrow keys.\n\n"
+						+ "Change the fit type by clicking a datapoint and selecting 'fit'\n"
+						+ "Export by using File+Export or clicking a datapoint\n" + "and selecting 'Export'.\n\n"
+						+ "Datapoints can be moved or deleted.");
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		if ((e.getKeyCode() == KeyEvent.VK_V)
+				&& ((e.getModifiers() & KeyEvent.CTRL_MASK) != 0 && tabbedPane.getSelectedIndex() == 0)) {
+			figure.getImageFromClipboard();
+			return;
+		}
+		switch (e.getKeyCode()) {
+
+		case KeyEvent.VK_UP:
+			figure.movepoint(0, -1);
+			break;
+		case KeyEvent.VK_DOWN:
+			figure.movepoint(0, 1);
+			break;
+		case KeyEvent.VK_LEFT:
+			figure.movepoint(-1, 0);
+			break;
+		case KeyEvent.VK_RIGHT:
+			figure.movepoint(1, 0);
+			break;
+
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		boolean isrightbutton = (e.getModifiers() & InputEvent.BUTTON3_MASK) == InputEvent.BUTTON3_MASK;
+		figure.addpoint(e.getX(), e.getY(), isrightbutton);
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+	}
+
 }
-  
- }
